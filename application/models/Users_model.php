88de60bed80d4715c3d9d628b11ed7583956c56d
afendi <afendiajip94@gmail.com>
2019-09-11 09:07:40 +0700

    first commit

---
+++ application/models/Users_model.php
@@ -0,0 +1,15 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+	class Users_model extends CI_Model {
+		function __construct(){
+			parent::__construct();
+			$this->load->database();
+		}
+ 
+		public function login($email, $password){
+			$query = $this->db->get_where('users', array('email'=>$email, 'password'=>$password));
+			return $query->row_array();
+		}
+ 
+	}
+?>
